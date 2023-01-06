a43732b12c2d8de0d4c2c82a375594b524c6b30d
sabdahtb <sabdahutabarat2804@gmail.com>
2023-01-06 17:03:55 +0700

    init and test husky

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended'
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    "ecmaVersion": 7,
+    "sourceType": "module"
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['state', 'config']
+      }
+    ]
+  },
+  settings: {}
+}
