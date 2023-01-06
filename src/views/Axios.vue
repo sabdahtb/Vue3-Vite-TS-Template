222f194fd05aaee6484faf4b491d9437d9d809d1
sabdahtb <sabdahutabarat2804@gmail.com>
2023-01-06 16:01:43 +0700

    init axios with interceptors

--- src/views/Axios.vue
+++ src/views/Axios.vue
@@ -1,23 +1,73 @@
 <template>
-    <div class="home-container page-container">
-      <img class="vue-element-plus-logo" alt="Vue logo" src="../assets/vue.svg" />
-      <div class="page-title">Vue Vite TypeScript</div>
+  <div>
+    <div class="user-info-container">
+      <el-card>
+        <template #header>
+          <div>
+            <button class="button" @click="getUserInfo">
+              get info
+            </button>
+          </div>
+        </template>
+        <div v-loading="loading">
+          <div v-if="userInfo?.name">name: {{ userInfo?.name }}</div>
+          <div v-if="userInfo?.bio">bio: {{ userInfo?.bio }}</div>
+        </div>
+      </el-card>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue'
-  
-  export default defineComponent({
-    name: 'Axios'
-  })
-  </script>
-  
-  <style scoped lang="stylus">
-  .home-container {
-    .vue-element-plus-logo {
-      width 50%
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue'
+import axios from '../utils/axios'
+
+export default defineComponent({
+  name: 'Axios',
+  setup() {
+    const userInfo: Ref = ref(null)
+    const loading = ref(false)
+
+    const getUserInfo = () => {
+      loading.value = true
+      axios
+        .get('/users/sabdahtb')
+        .then((response) => {
+          // eslint-disable-next-line no-console
+          console.log('response: ', response.data)
+          userInfo.value = response.data
+          loading.value = false
+        })
+        .catch((error) => {
+          loading.value = false
+          // eslint-disable-next-line no-console
+          console.error(error)
+        })
+    }
+
+    return {
+      userInfo,
+      loading,
+      getUserInfo
     }
   }
-  </style>
-  
+})
+</script>
+
+<style scoped lang="stylus">
+
+  .user-info-container {
+    padding 10px
+    border 1px solid #d5d5d5
+    width 300px
+    border-radius 5px
+
+    .button {
+      padding 5px 15px
+      border-radius 5px
+      margin-bottom 12px
+      border 1px solid #d5d5d5
+    }
+
+  }
+</style>
